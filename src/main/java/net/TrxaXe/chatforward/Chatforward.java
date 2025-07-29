1e27639c3c098e6339ca1c78fdb8eb7708a3a9bc
xloopp <1991315247@qq.com>
2025-07-29 15:59:45 +0800

    t

---
+++ src/main/java/net/TrxaXe/chatforward/Chatforward.java
@@ -0,0 +1,10 @@
+package net.TrxaXe.chatforward;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Chatforward implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/TrxaXe/chatforward/client/ChatforwardClient.java
@@ -0,0 +1,10 @@
+package net.TrxaXe.chatforward.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChatforwardClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
